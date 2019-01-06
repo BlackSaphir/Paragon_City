1cec766ddd298791a1fef169e64aec7cf51ef42d
DESKTOP - Carsten <carsten.dollack@yahoo.de>
2019-01-06 17:01:40 +0100

    [UPDATED] GameInstance SaveGame

--- Source/Paragon_City/Paragon_GameInstance.cpp
+++ Source/Paragon_City/Paragon_GameInstance.cpp
@@ -1,11 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Paragon_GameInstance.h"
+#include "Paragon_SaveGame.h"
+#include "Kismet/GameplayStatics.h"
 
 
 
-UParagon_GameInstance::UParagon_GameInstance(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+UParagon_GameInstance::UParagon_GameInstance()
+	: SaveSlot(TEXT("SaveGame")),
+	SaveUserIndex(0)
 {
+}
+
+void UParagon_GameInstance::CheckSaveGame()
+{
+	CurrentSaveGame = nullptr;
+
+	if (UGameplayStatics::DoesSaveGameExist(SaveSlot, SaveUserIndex))
+	{
+		CurrentSaveGame = Cast<UParagon_SaveGame>(UGameplayStatics::LoadGameFromSlot(SaveSlot, SaveUserIndex));
+	}
+	else
+	{
+		CurrentSaveGame = Cast<UParagon_SaveGame>(UGameplayStatics::CreateSaveGameObject(UParagon_SaveGame::StaticClass()));
+	}
+
 
 }
 
+void UParagon_GameInstance::WriteSaveGame()
+{
+	UGameplayStatics::SaveGameToSlot(CurrentSaveGame, SaveSlot, SaveUserIndex);
+}
